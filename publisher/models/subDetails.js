faa4b875ebc292e436e0fcf2c4958fd327713b73
ejiro-edwin <ejiroedwin@gmail.com>
2021-02-27 23:56:02 +0100

    first commit

---
+++ publisher/models/subDetails.js
@@ -0,0 +1,36 @@
+const {Schema, model} = require('mongoose')
+const SubdetailsSchema = new Schema({
+    topic: {
+        type: String,
+        required: true
+
+    },
+    url: {
+        type: String,
+        required: true
+    },
+    isDelete: {
+        type: Boolean,
+        default: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    updatedAt: {
+        type: Date
+    }
+}, {
+    timestamps: true
+}, {
+    toObject: {
+        virtuals: true
+    },
+    toJSON: {
+        virtuals: true
+    }
+})
+
+const Subdetails = model('Subdetails', SubdetailsSchema)
+
+module.exports = Subdetails
